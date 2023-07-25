3324c99174568f0b0263f7da1e436b3bb6a677fe
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-07-25 11:35:14 -0300

    Enviando e recebendo Json na API

---
+++ src/main/java/com/remedios/leo/estudo/controllers/remedioController.java
@@ -0,0 +1,16 @@
+package com.remedios.leo.estudo.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remedios")
+public class remedioController {
+
+	@PostMapping
+	public void cadastrar(@RequestBody String json) {
+		System.out.println( json);
+	}
+}
