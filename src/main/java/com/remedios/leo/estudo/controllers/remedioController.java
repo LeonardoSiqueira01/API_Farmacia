5c3bc64f08a256e1a839cea4a1c2c42a5d31b0a0
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-07-28 13:43:52 -0300

    Criando metodo delete no rest controller

--- src/main/java/com/remedios/leo/estudo/controllers/remedioController.java
+++ src/main/java/com/remedios/leo/estudo/controllers/remedioController.java
@@ -3,7 +3,9 @@ package com.remedios.leo.estudo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,5 +44,11 @@ public class remedioController {
 	public void AtualizarRemedio(@RequestBody @Valid DadosAtualizacaoRemedio dados) {
 		var remedio = Repository.getReferenceById(dados.id());
 		remedio.atualizarInformacoes(dados);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deletarRemedio(@PathVariable Long id) {
+		Repository.deleteById(id);
 	}
 }
