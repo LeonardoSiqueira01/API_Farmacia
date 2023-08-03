c3e892eab16db996a5bf5ffeb426e9926e8f0faa
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-08-03 11:23:02 -0300

    Implementando a interface JPA Repository e tambem a interface UserDetailService do spring security

--- src/main/java/com/remedios/leo/estudo/usuarios/Usuario.java
+++ src/main/java/com/remedios/leo/estudo/usuarios/Usuario.java
@@ -1,5 +1,7 @@
 package com.remedios.leo.estudo.usuarios;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -40,6 +42,23 @@ public class Usuario {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
 	}
 
 	
